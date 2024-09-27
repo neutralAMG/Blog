4f34e5cfe3ec403d3a3e4438c44da49b07269cbd
amoscoso <amoscoso@gmail.com>
2024-09-27 15:56:35 -0400

    s

---
+++ Blog.Infraestructure.Presistance/Extensions/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blog.Infraestructure.Presistance.Extensions
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddInfraestructurePersistanceLayer(this IServiceCollection services, IConfiguration confi)
+        {
+            return services;
+        }
+    }
+}
